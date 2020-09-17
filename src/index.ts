7cfaac489aa7791a3f1c87d53b2cdb63af4dfc92
joris <jorisvddonk@gmail.com>
2020-09-17 21:31:32 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Parse an uqm text localization file, and return the relevant string identifiers with their localized texts.
+ * Localization files are used in various places in UQM, most notably in conversations with aliens.
+ * Localizations _may_ have audio files associated with them.
+ * @param str file contents to parse
+ */
+export function parseTextLocalizationFile(str: string) {
+  // todo
+}
+
+/**
+ * Parse an uqm speech timing file, and return speech timings for the various identifiers.
+ * Speech timing files are used to sync up text to voice audio files in game.
+ * @param str file contents to parse
+ */
+export function parseSpeechTimingFile(str: string) {
+  // todo
+}
+
